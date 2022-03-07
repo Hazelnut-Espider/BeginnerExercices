21f8b8d1ecf4a2f1a84f57e3f6483f64fa664cd8
Elder Costa <costaredle@gmail.com>
2022-03-07 19:21:29 +0000

    building a Step Accounter
    it can be used as a countdown step as well

---
+++ aula005ex/script.js
@@ -0,0 +1,30 @@
+function contar(){
+    let ini = window.document.getElementById('qnt')
+    let fim = window.document.getElementById('fim')
+    let passo = window.document.getElementById('passos')
+    let res = window.document.getElementById('res')
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = 'Impossivel contar!'
+        //window.alert('[ERRO] faltam dados!')
+    }else{
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p <= 0){
+            window.alert('Passo Invalido! Considerando Passo 1')
+            p = 1
+        }
+        if(i < f){
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }else { //contagem regressiva
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
